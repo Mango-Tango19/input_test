f65b201e20c9adc364f2395bef3e3d3ebdabb66b
Дарья Булатова <darabulatova@Daras-Air.Dlink>
2022-02-10 22:01:10 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+// 1. Починить инпуты;
+// 2. Сделать так что бы инпуты добавлялись
+
+export default function App() {
+  const handleSubmit = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bob Form</h2>
+      <div>
+        <input type="text" value="bob" />
+        <button type="button">Remove input</button>
+      </div>
+      <div>
+        <input type="text" value="не bob" />
+        <button type="button">Remove input</button>
+      </div>
+      <div>
+        <input type="text" value="bob" />
+        <button type="button">Remove input</button>
+      </div>
+      <div>
+        <input type="text" value="не bob" />
+        <button type="button">Remove input</button>
+      </div>
+      <div className="controls">
+        <button type="button">Add input</button>
+        <button onClick={handleSubmit} type="button">
+          Submit values
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
